98cdf9d51e266816438e4c9941f01089f723889e
KiberInfinity <KiberInfinity@gmail.com>
2012-01-01 18:50:24 +0000

    git-svn-id: https://vkopt.googlecode.com/svn/trunk@118 abbab872-c1ea-7929-a236-346daee65547

--- source/vk_lib.js
+++ source/vk_lib.js
@@ -274,44 +274,45 @@ if (!window.Audio){
 }
 /* FUNCTIONS. LEVEL 1 */
 	//LANG
-	function print_r( array, return_val ) {
-		var output = "", pad_char = " ", pad_val = 4;
+   function print_r( array, return_val ) {
+      var output = "", pad_char = " ", pad_val = 4;
 
-		var formatArray = function (obj, cur_depth, pad_val, pad_char) {
-			if(cur_depth > 0)
-				cur_depth++;
+      var formatArray = function (obj, cur_depth, pad_val, pad_char) {
+         if(cur_depth > 0)
+            cur_depth++;
 
-			var base_pad = repeat_char(pad_val*cur_depth, pad_char);
-			var thick_pad = repeat_char(pad_val*(cur_depth+1), pad_char);
-			var str = "";
+         var base_pad = repeat_char(pad_val*cur_depth, pad_char);
+         var thick_pad = repeat_char(pad_val*(cur_depth+1), pad_char);
+         var str = "";
 
-			if(typeof obj=='object' || typeof obj=='array') {
-				str += '[\n';//"Array\n" + base_pad + "(\n";
-				for(var key in obj) {
-					if(typeof obj[key]=='object' || typeof obj[key]=='array') {
-						str += thick_pad + ""+key+": "+formatArray(obj[key], cur_depth+1, pad_val, pad_char);
-					} else {
-						str += thick_pad + ""+key+": " + obj[key] + "\n";
-					}
-				}
-				str += base_pad + "]\n";
-			} else {
-				str = obj.toString();
-			};
+         if(typeof obj=='object' || typeof obj=='array' || (obj.length>0 && typeof obj!='string' && typeof obj!='number')) {
+            if(!(typeof obj=='object' || typeof obj=='array'))str = '\n'+obj.toString()+'\n';
+         str += '[\n';//"Array\n" + base_pad + "(\n";
+            for(var key in obj) {
+               if(typeof obj[key]=='object' || typeof obj[key]=='array' || (obj.length>0 && typeof obj!='string' && typeof obj!='number')) {
+             str += thick_pad + ""+key+": "+((!(typeof obj=='object' || typeof obj=='array'))?'\n'+obj[key]+'\n':'')+formatArray(obj[key], cur_depth+1, pad_val, pad_char)+'\n';
+               } else {
+                  str += thick_pad + ""+key+": " + obj[key] + "\n";
+               }
+            }
+            str += base_pad + "]\n";
+         } else {
+            str = obj.toString();
+         };
 
-			return str;
-		};
+         return str;
+      };
 
-		var repeat_char = function (len, char) {
-			var str = "";
-			for(var i=0; i < len; i++) { str += char; };
-			return str;
-			return str;
-		};
+      var repeat_char = function (len, char) {
+         var str = "";
+         for(var i=0; i < len; i++) { str += char; };
+         return str;
+         return str;
+      };
 
-		output = formatArray(array, 0, pad_val, pad_char);
-			return output;
-	}
+      output = formatArray(array, 0, pad_val, pad_char);
+         return output;
+   }
 	
 	function vkCutBracket(s,bracket){
 		if (CUT_VKOPT_BRACKET || bracket==2) s=(s.substr(0,1)=='[')?s.substr(1,s.length-2):s;
