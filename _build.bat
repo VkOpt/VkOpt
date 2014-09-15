c6ca7ac171a116def00e19b9f8fbf4d170ff8051
EgoAkk <EgoAkk@gmail.com>
2014-09-15 17:32:47 +0300

    Фикс создания файла вместо копирования в папку, если папка отсутсвует

--- _build.bat
+++ _build.bat
@@ -1,17 +1,17 @@
 @echo off
-copy source builds\chrome\scripts
-copy source builds\firefox\scripts
-copy source builds\vkopt.safariextension\scripts
-copy source builds\opera.extension\scripts
+copy source builds\chrome\scripts\
+copy source builds\firefox\scripts\
+copy source builds\vkopt.safariextension\scripts\
+copy source builds\opera.extension\scripts\
 
-copy background.js builds\chrome
-copy background.js builds\firefox\chrome\content
-copy background.js builds\maxthon
-copy background.js builds\opera.extension
-copy background.js builds\vkopt.safariextension
+copy background.js builds\chrome\
+copy background.js builds\firefox\chrome\content\
+copy background.js builds\maxthon\
+copy background.js builds\opera.extension\
+copy background.js builds\vkopt.safariextension\
 
-copy content_script.js builds\chrome
-copy content_script.js builds\firefox\chrome\content
-copy content_script.js builds\maxthon
-copy content_script.js builds\opera.extension\includes
-copy content_script.js builds\vkopt.safariextension
+copy content_script.js builds\chrome\
+copy content_script.js builds\firefox\chrome\content\
+copy content_script.js builds\maxthon\
+copy content_script.js builds\opera.extension\includes\
+copy content_script.js builds\vkopt.safariextension\
