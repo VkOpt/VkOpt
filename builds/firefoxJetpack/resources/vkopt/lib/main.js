22e1542920602c3275eaf0c55fddd97fa9317e91
pmmlabs <kilowatt36@mail.ru>
2015-03-23 17:44:13 +0300

    уточнение условий pagemod для джетпака

--- builds/firefoxJetpack/resources/vkopt/lib/main.js
+++ builds/firefoxJetpack/resources/vkopt/lib/main.js
@@ -3,7 +3,9 @@ var self = require("sdk/self");
 //var {Cc, Ci} = require("chrome");
 
 pageMod.PageMod({
-  include: [ /.*vk\.com.*/ ],
-  contentScriptFile: [ self.data.url("loader.js") ]
+    include: [ "*.vkontakte.ru", "*.vk.com" ],
+    exclude: /.*notifier\.php|.*im_frame\.php/i,
+    contentScriptFile: self.data.url("loader.js"),
+    contentScriptWhen: "ready"
 });
 
