fb3e0e13ef5da9fcc06b25b779ad7d62a9eb2f60
Pmmlabs <kilowatt36@mail.ru>
2015-10-27 20:08:19 +0300

    Точный поиск по аудио

--- source/vk_media.js
+++ source/vk_media.js
@@ -6510,6 +6510,8 @@ vk_au_down={
          return;
       }
       //vkaddcss('#vk_mp3_links_area, #vk_m3u_playlist_area,#vk_pls_playlist_area, #vk_mp3_wget_links_area{width:520px; height:400px;}');
+      if (val(ge('audioExactSearch')))
+          return vk_audio.links_to_audio_on_page(); // В случае точного поиска получить ссылки со страницы
       var params={}; 
       if (cur.album_id && cur.album_id>0) params['album_id']=cur.album_id;
       var box=vkAlertBox('',vkBigLdrImg);
@@ -6528,7 +6530,7 @@ vk_au_down={
           params["count"] = 300;        // Максимум аудиозаписей (ограничение ВК)
           if (cur.autoComplete)         // Исправление ошибок. На практике, true при живом поиске и false при обновлении страницы
               params["auto_complete"] = 1;
-          params["sort"] = 2;           // Сортировка по популярноси. Стандартная у вконтакта.
+          params["sort"] = 2;           // Сортировка по популярности. Стандартная у вконтакта.
       }
       dApi.call(audio_method,params,function(r){
          var res='#EXTM3U\n';
@@ -6777,4 +6779,50 @@ if (!window.vkscripts_ok) window.vkscripts_ok=1; else window.vkscripts_ok++;
       }
    };
    if (window.vkopt_ready && browser.mozilla) vkopt_plugin_run(PLUGIN_ID);
+})();
+
+(function () {
+    var PLUGIN_ID = 'ExactAudioSearch';
+    vkopt_plugins[PLUGIN_ID] = {
+        Name: 'Exact Audio Search',
+        query: '',      // поисковый запрос
+        performer: 0,   // поиск по исполнителю
+        onLocation: function (nav_obj, cur_module_name) {
+            if (cur_module_name == 'audio')
+                this.UI();
+        },
+        UI: function () {
+            if (!ge('audioExactSearch')) { // создание галочки "искать в точности"
+                var parent = ge('pad_album_filters') || ge('album_filters');
+                var el = vkCe('div', {'class': 'audio_filter'}, '<div id="audioExactSearch" class="label"></div>');
+                parent.insertBefore(el, domFC(parent));
+                stManager.add('ui_controls.js', function () {
+                    new Checkbox(ge('audioExactSearch'), {
+                        checked: false,
+                        label: IDL('searchExactly')
+                    });
+                });
+            }
+        },
+        filter: function (div) {    // чистка контейнера с аудиозаписями div от лишних аудио
+            var audios = geByClass('audio', div);
+            for (var i in audios) {
+                var performer = geByTag('b', audios[i])[0].textContent.toLowerCase().trim();
+                var title = geByClass('title', audios[i])[0].textContent.toLowerCase().trim();
+                if (vkopt_plugins[PLUGIN_ID].performer == 1 && performer != vkopt_plugins[PLUGIN_ID].query
+                    || vkopt_plugins[PLUGIN_ID].performer == 0 && title != vkopt_plugins[PLUGIN_ID].query && performer + ' - ' + title != vkopt_plugins[PLUGIN_ID].query)
+                    re(audios[i]);
+            }
+        },
+        onResponseAnswer: function (answer, url, params) {  // Обработка поискового запроса
+            if (val(ge('audioExactSearch')) && url == '/audio' && params.act == 'search') {
+                this.performer = params.performer;    // поиск по исполнителю
+                this.query = params.q.toLowerCase().trim();  // поисковый запрос (регистронезависимый)
+                answer[0] = vkModAsNode(answer[0], this.filter);
+                answer[1] = vkModAsNode(answer[1], this.filter);
+            } else if (url == '/pads.php' && params.pad_id == 'mus')
+                setTimeout(vkopt_plugins[PLUGIN_ID].UI, 1);
+        }
+    };
+    if (window.vkopt_ready) vkopt_plugin_run(PLUGIN_ID);
 })();
