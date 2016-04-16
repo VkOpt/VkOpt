e5241fce3b9a77c7531cbb068304380039540f58
Bfgeshka <bfg@derpy.ru>
2016-04-16 21:00:39 +0900

    correcting js paths

--- _build.sh
+++ _build.sh
@@ -1,22 +1,36 @@
 #! /bin/sh
 
-DIR_CHR=builds/chrome
-DIR_FFX=builds/firefox
-DIR_SFR=builds/vkopt.safariextension
-DIR_OPR=builds/opera.extension
-DIR_FJT=builds/firefoxJetpack/resources/vkopt/data
-
 BCGJS=background.js
 CNTJS=content_script.js
 
+
+# Chrome
+DIR_CHR=builds/chrome
+cp $BCGJS $CNTJS $DIR_CHR
 cp source/* $DIR_CHR/scripts
+
+# Firefox
+DIR_FFX=builds/firefox
+cp $BCGJS $DIR_FFX/resources/vkopt/lib
+cp $CNTJS $DIR_FFX/resources/vkopt/data
 cp source/* $DIR_FFX/scripts
-cp source/* $DIR_SFR/scripts
-cp source/* $DIR_OPR/scripts
+
+# Firefox (jetpack)
+DIR_FJT=builds/firefoxJetpack/resources/vkopt/data
+cp $BCGJS $CNTJS $DIR_FJT
 cp source/* $DIR_FJT/scripts
 
-cp $BCGJS $CNTJS $DIR_CHR
-cp $BCGJS $CNTJS $DIR_FFX
+# Safari
+DIR_SFR=builds/vkopt.safariextension
 cp $BCGJS $CNTJS $DIR_SFR
-cp $BCGJS $CNTJS $DIR_OPR
-cp $BCGJS $CNTJS $DIR_FJT
+cp source/* $DIR_SFR/scripts
+
+# Opera
+DIR_OPR=builds/opera.extension
+cp $BCGJS $DIR_OPR
+cp $CNTJS $DIR_OPR/includes
+cp source/* $DIR_OPR/scripts
+
+# Maxthon
+DIR_MXT=builds/maxthon
+cp $BCGJS $CNTJS $DIR_MXT
