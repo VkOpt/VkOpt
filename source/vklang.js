fea100ecf9be483c5af395efd4660e194fbfef6c
KiberInfinity <kiberinfinity@gmail.com>
2018-04-01 01:32:17 +0200

    Правки для функции показа количества онлайн пользователей в сети

--- source/vklang.js
+++ source/vklang.js
@@ -781,6 +781,7 @@ vk_lang_ru={
    ,"seOldAudioButtons":"\u041a\u043b\u0430\u0441\u0441\u0438\u0447\u0435\u0441\u043a\u0438\u0439 \u0441\u0442\u0438\u043b\u044c \u043d\u0430\u0437\u0432\u0430\u043d\u0438\u0439 \u0438 \u043a\u043d\u043e\u043f\u043e\u043a \u043f\u0440\u043e\u0438\u0433\u0440\u044b\u0432\u0430\u043d\u0438\u044f \u0430\u0443\u0434\u0438\u043e"
    ,"seShiftPageButtons": "\u041f\u043e\u043a\u0430\u0437\u044b\u0432\u0430\u0442\u044c \u0432 \u0448\u0430\u043f\u043a\u0435 \u043a\u043d\u043e\u043f\u043a\u0438 \u0441\u0434\u0432\u0438\u0433\u0430 \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u044b"
    ,"seHideShiftBtn": "\u0421\u0434\u0435\u043b\u0430\u0442\u044c \u043a\u043d\u043e\u043f\u043a\u0438 \u0441\u0434\u0432\u0438\u0433\u0430 \u043f\u043e\u043b\u043d\u043e\u0441\u0442\u044c\u044e \u043f\u0440\u043e\u0437\u0440\u0430\u0447\u043d\u044b\u043c\u0438"
+   ,"online_count":['', ' %s \u0432 \u0441\u0435\u0442\u0438', '%s \u0432 \u0441\u0435\u0442\u0438', '%s \u0432 \u0441\u0435\u0442\u0438']
    };
 
 vk_lang_en={//by Hzy
@@ -1712,6 +1713,7 @@ vk_lang_en={//by Hzy
   ,"seOldAudioButtons": "Classic audios titles and play buttons style"
   ,"seShiftPageButtons": "Show page shift position buttons in the header"
   ,"seHideShiftBtn": "Make shift buttons fully transparent"
+  ,"online_count":['', ' %s online', ' %s online', ' %s online']
 };
 
 vk_lang_ua={//by Vall (id3476823) and Vall_gorr (id119992149)
