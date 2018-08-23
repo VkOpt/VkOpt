fb2d2779cd9a768a0ff86a5862b8771e425789d0
KiberInfinity <kiberinfinity@gmail.com>
2018-08-23 02:31:40 +0200

    v3.0.6.2 (build 180822)

--- source/vkopt.js
+++ source/vkopt.js
@@ -11,8 +11,8 @@
 //
 /* VERSION INFO */
 var vVersion	= 306;
-var vBuild = 180810;
-var vVersionRev = 1;
+var vBuild = 180822;
+var vVersionRev = 2;
 var vPostfix = '';
 
 if (!window.vkopt) window.vkopt={};
