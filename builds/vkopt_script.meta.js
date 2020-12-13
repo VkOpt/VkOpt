3df346f7aad97a21784323ba53fe453837065523
clownless <m4x3r1337@yandex.ru>
2020-12-13 09:44:55 +0400

    v3.0.7.23 (191219) (Фикс поиска копий фото через яндекс)

--- builds/vkopt_script.meta.js
+++ builds/vkopt_script.meta.js
@@ -1,11 +1,11 @@
 ﻿// ==UserScript==
 // @name          VKOpt
-// @version       3.0.7.22
+// @version       3.0.7.23
 // @author        KiberInfinity [id13391307]
 // @namespace     http://vkopt.net/
 // @description   Vkontakte Optimizer 3.x
-// @downloadUrl   https://raw.githubusercontent.com/m4x3r/VkOpt/master/builds/vkopt_script.user.js
-// @updateUrl     https://raw.githubusercontent.com/m4x3r/VkOpt/master/builds/vkopt_script.meta.js
+// @downloadUrl   https://raw.githubusercontent.com/clownless/VkOpt/master/builds/vkopt_script.user.js
+// @updateUrl     https://raw.githubusercontent.com/clownless/VkOpt/master/builds/vkopt_script.meta.js
 // @match       *://vkontakte.ru/*
 // @match       *://*.vkontakte.ru/*
 // @match       *://vk.com/*
